7709572a2dd815fe417a8d430164d5c713a0f6f6
rajmahajan98 <47290275+rajmahajan98@users.noreply.github.com>
2025-07-27 19:04:44 +0530

    Updated Seach API

--- src/utlis/config.js
+++ src/utlis/config.js
@@ -2,7 +2,7 @@ const API_KEY = 'AIzaSyDr8pQNrpZ3AXFAKAaXu1CzL1kWVqcSzIk';
 
 export const YOUTUBE_VIDEO_API = 'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key='+ API_KEY;
 
-export const YOUTUBE_SEARCH_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+export const YOUTUBE_SEARCH_API = "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
 export const OFFSET_LIVECHAT = 10;
 
